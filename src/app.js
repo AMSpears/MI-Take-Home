0f686eec679c0ac4b061b189b7ac241d4c911c52
Angie Spears <angie.spears809@gmail.com>
2020-09-27 20:25:15 -0400

    separate concerns

--- src/app.js
+++ src/app.js
@@ -6,36 +6,8 @@ import snow from "../img/snow.png"
 import sunny from "../img/sunny.png"
 
 
-const fetchAllData = async () => {
+const addData = (initialDays, city) => {
   const currDate = new Date()
-  const month =
-    currDate.getMonth() + 1 < 10
-      ? `0${currDate.getMonth() + 1}`
-      : currDate.getMonth() + 1;
-  const day =
-    currDate.getDay() < 10
-    ? `0${currDate.getDay()}`
-    : currDate.getDay()
-  const year = currDate.getFullYear()
-  let city = ''
-  let lat = 0
-  let long = 0
-
-  // CITY DATA
-  const stateZipcode = 20001
-  const response = await axios.get(
-    `https://se-weather-api.herokuapp.com/api/v1/geo?zip_code=${stateZipcode}`
-  )
-  city = `${response.data.city}, ${response.data.regionCode}`
-  lat = Number(response.data.latitude).toFixed(2)
-  long = Number(response.data.longitude).toFixed(1)
-
-
-  // CITY WEATHER DETAILS
-  const detailsResponse = await axios.get(`https://se-weather-api.herokuapp.com/api/v1/forecast?latitude=${lat}&longitude=${long}&date=${month}/${day}/${year}`)
-  const initialDays = detailsResponse.data.daily.data.filter( (data, idx) => idx < 3)
-
-
   const title = document.getElementById('title')
   const subTitle = document.getElementsByClassName('weekday')
   const iconImg = document.getElementsByTagName('img')
@@ -44,11 +16,10 @@ const fetchAllData = async () => {
 
   title.innerText = `WEATHER FORECAST FOR ${city.toUpperCase()}`
 
-  const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday','Friday', 'Saturday', 'Sunday']
+  const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-
-  weekdays.map( (weekday, idx) => {
-    if (idx === currDate.getDay() - 1) {
+  weekdays.map((weekday, idx) => {
+    if (idx === 0) {
       subTitle[0].innerText = 'Today:'
     }
     if (idx === currDate.getDay()) {
@@ -59,7 +30,7 @@ const fetchAllData = async () => {
     }
   })
 
-  initialDays.map( (day, idx) => {
+  initialDays.map((day, idx) => {
     if (day.icon === 'cloudy') {
       iconImg[idx].src = cloudy
       weatherDescription[idx].innerText = `${day.icon.slice(0, 1).toUpperCase()}${day.icon.slice(1)}`
@@ -81,6 +52,40 @@ const fetchAllData = async () => {
       weatherTemp[idx].innerText = `${day.temperatureHigh.toFixed(0)}°/ ${day.temperatureLow.toFixed(0)}° F`
     }
   })
+}
+
+const fetchAllData = async () => {
+  let currDate = new Date()
+  currDate = convertDate(currDate)
+  // const month =
+  //   currDate.getMonth() + 1 < 10
+  //     ? `0${currDate.getMonth() + 1}`
+  //     : currDate.getMonth() + 1;
+  // const day =
+  //   currDate.getDay() < 10
+  //   ? `0${currDate.getDay()}`
+  //   : currDate.getDay()
+  // const year = currDate.getFullYear()
+  let city = ''
+  let lat = 0
+  let long = 0
+
+  // CITY DATA
+  const stateZipcode = 20001
+  const response = await axios.get(
+    `https://se-weather-api.herokuapp.com/api/v1/geo?zip_code=${stateZipcode}`
+  )
+  city = `${response.data.city}, ${response.data.regionCode}`
+  lat = Number(response.data.latitude).toFixed(2)
+  long = Number(response.data.longitude).toFixed(1)
+
+
+  // CITY WEATHER DETAILS
+  const detailsResponse = await axios.get(`https://se-weather-api.herokuapp.com/api/v1/forecast?latitude=${lat}&longitude=${long}&date=${currDate}`)
+  const initialDays = detailsResponse.data.daily.data.filter( (data, idx) => idx < 3)
+
+  addData(initialDays, city)
+
 }
 
 fetchAllData()
